c503c54b91f15395e135b3306c045c7f9ee02100
ibrahimKhan <ikkhan007m@gmail.com>
2025-03-26 15:48:42 +0500

    new commit

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      "/api":import.meta.env.VITE_backend_base_Url
-    }
-  }
+
 })

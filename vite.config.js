37c5e6515c5319c3b44d2630c7a20b3b90093c6e
ibrahimKhan <ikkhan007m@gmail.com>
2025-03-26 16:09:03 +0500

    new commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  server:{
+    proxy:{
+      "/api":"https://lms-backend-five-kohl.vercel.app"
+    }
+  }
 })
